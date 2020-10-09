3872674318f86c1621036c7ce9dd5612331845b8
gauravpks <66349530+gauravpks@users.noreply.github.com>
2020-10-09 13:20:10 +0530

    Add files via upload

---
+++ sample1.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  9 12:42:45 2020
+
+@author: gaurav.pr
+"""
+
+import os
+
+def mostimportantfunction():
+    print("I am a coder")
+
+print(__name__)
+
+if(__name__=="__main__"):
+
+    print("-----------------------------------------------------")
+    print(os.listdir("/"))
+    print("-----------------------------------------------------")
+    print("I am the king of India")
+    
+    print("-----------------------------------------------------")
+
+#mostimportantfunction()
---
+++ sample2.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  9 12:47:14 2020
+
+@author: gaurav.pr
+"""
+
+import sample1
+
+sample1.mostimportantfunction()
